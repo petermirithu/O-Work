a1aeae76749748ca08c5665140c64ee196e1b032
Josephat-n <josphatnjoroge254@gmail.com>
2019-10-10 17:43:39 +0300

    create a user constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
+//Business logic 
+function Person(fname, lname,uname, email,password, pass2,){
+  this.fname = fname;
+  this.lname= lname;
+  this.uname = uname;
+  this.email = email;
+  this.password = password;
+  this.pass2 = pass2;
+}
+
+
+
+
+// User interface logic
 $(document).ready(function() {
   setInterval(function() {
       $("#message").animate({width: '100%',height: '100%'}, 500);
       $("#message").animate({ width: '95%',height: '95%'}, 600); 
   },1000);    
-});
+});//end of document.get.ready function
